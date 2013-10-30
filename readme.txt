5fd7344592d42619b3c281d429b1e7af2d3af1c2
vinoj.cardoza <vinoj.cardoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-30 09:48:58 +0000

    First Version
    git-svn-id: https://plugins.svn.wordpress.org/wps-google-analytics/trunk@795797 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Facebook Like Box ===
+Contributors: vinoj.cardoza
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=vinoj%2ecardoza%40gmail%2ecom&currency_code=GBP&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
+Tags: google, analytics, google analytics
+Requires at least: 3.0
+Tested up to: 3.6.1
+Stable tag: trunk
+License: GPLv2 or later
+
+WPS Google Analytics allows you to easilly add your Google Analytics code through out the whole site.
+
+== Description ==
+
+WPS Google Analytics allows you to easilly add your Google Analytics code through out the whole site. Just add your ID, choose if you are on a sub-domain (setting in Google Analytics), and enter the domain.
+
+WPS Google Analytics will not track admin users logged-in.
+
+If you have any question, you can find the plugin page here : http://www.vinojcardoza.com/blog/wps-google-analytics
+
+= Donations =
+
+Thanks for downloading and installing my plugin. You can show your appreciation and support for future development by contributing a small donation.
+
+= Translation =
+Translation is enabled for this plugin. If someone interested in translating this plugin please visit the following link and post in the comments section with your contact email. Thanks.
+
+http://www.vinojcardoza.com/blog/wps-google-analytics
+
+= Translated languages =
+
+* French
+
+== Installation ==
+
+1. Download the plugin.
+2. Upload to your blog (/wp-content/plugins/).
+3. Activate it.
+4. Click the Google Analytics menu.
+5. Fill in the options.
+
+You're done!
+
+Uninstalling is as simple as deactivating the plugin.
+
+== Screenshots ==
+
+1. screenshot-1.gif
---
+++ wps-google-analytics.php
@@ -0,0 +1,150 @@
+<?php
+/*
+Plugin Name: WPS Google Analytics
+Plugin URI: http://www.vinojcardoza.com/blog/wps-google-analytics/
+Description: WPS Google Analytics allows you to easilly add your Google Analytics code through out the whole site.  
+Version: 1.0
+Author: Vinoj Cardoza
+Author URI: http://www.vinojcardoza.com
+License: GPL2
+*/
+
+add_action("plugins_loaded", "cawpsga_init");
+add_action("admin_menu", "cawpsga_menu");
+add_action("wp_enqueue_scripts", "put_ga_code");
+
+function put_ga_code()
+{
+	$ga_track_id = get_option('cawps_ga_track_id');
+	$sub_domain = get_option('cawps_ga_sub_domain');
+	$sub_domain_url = get_option('cawps_ga_sub_domain_url');
+	$allow_multiple_domain = get_option('cawps_ga_multiple_domain');
+	
+	print "<script type='text/javascript'>";
+	print "var _gaq = _gaq || [];";
+	print "_gaq.push(['_setAccount', '".$ga_track_id."']);";
+	if($sub_domain == 'yes')
+	{
+		if(!empty($sub_domain_url))
+		{
+			print "_gaq.push(['_setDomainName', '".$sub_domain_url."']);";
+			if($allow_multiple_domain == 'yes')
+				print "_gaq.push(['_setAllowLinker', true])";
+		}		
+	}
+	print "_gaq.push(['_trackPageview']);";
+	print "(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>";		
+}
+
+function cawpsga_init(){
+	load_plugin_textdomain('cawpsgoogleanalytics', false, dirname( plugin_basename(__FILE__)).'/languages');
+}
+
+function cawpsga_menu(){
+	
+	add_menu_page(
+		__('WPS Google Analytics'), 
+		__('Google Analytics'), 
+		'manage_options', 
+		'cawps-google-analytics', 
+		'cawps_google_analytics_options_page',
+		plugin_dir_url(__FILE__).'google-analytics-icon.png');    
+    /*add_submenu_page(
+            'cawps-google-analytics',
+            __('Stats'),
+            __('Stats'),
+            'manage_options',
+            'cawps_stats',
+			'cawps_stats');*/
+}
+
+function cawps_google_analytics_options_page(){
+	
+	if(isset($_POST['frm_submit'])){
+			
+		if(!empty($_POST['frm_track_id'])) 
+			update_option('cawps_ga_track_id', $_POST['frm_track_id']);
+		
+		if(!empty($_POST['frm_sub_domain'])) 
+			update_option('cawps_ga_sub_domain', $_POST['frm_sub_domain']);
+		
+		if(!empty($_POST['frm_sub_domain_url'])) 
+			update_option('cawps_ga_sub_domain_url', $_POST['frm_sub_domain_url']);
+		
+		if(!empty($_POST['frm_multiple_top_level'])) 
+			update_option('cawps_ga_multiple_domain', $_POST['frm_multiple_top_level']);
+?>
+		<div id="message" class="updated fade"><p><strong><?php _e('Options saved.', 'cawpsgoogleanalytics' ); ?></strong></p></div>
+<?php	
+	}
+	$track_id = get_option('cawps_ga_track_id');
+	$sub_domain = get_option('cawps_ga_sub_domain');
+	$sub_domain_url = get_option('cawps_ga_sub_domain_url');
+	$allow_multiple_domain = get_option('cawps_ga_multiple_domain');
+?>
+	<div class="wrap">
+		<table>
+			<tr>
+				<td><img src="<?php print plugin_dir_url(__FILE__).'google-analytics-icon-32.png';?>" /></td>
+				<td><h1><?php echo __("WPS Google Analytics", "cawpsgoogleanalytics");?></h1></td>
+			</tr>	
+		</table>
+		
+		<!-- Administration panel form -->
+		<form method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+		<table>
+			<tr>
+				<td><h2><?php _e('Settings', 'cawpsgoogleanalytics');?></h2></td>
+				<td></td>
+			</tr>
+			<tr height="35">
+				<td>
+					<h3><?php _e('Google Analytics Tracking ID', 'cawpsgoogleanalytics');?>:</h3>
+				</td>
+				<td>
+					<input type="text" name="frm_track_id" size="50" value="<?php echo get_option('cawps_ga_track_id');?>"/>
+				</td>
+			</tr>
+			<tr height="35">
+				<td>
+					<h3><?php _e('Is this site installed in sub domain?', 'cawpsgoogleanalytics');?>:</h3>
+				</td>
+				<td>
+					<select name="frm_sub_domain" style="width:75px;">
+						<option value="yes"<?php if($sub_domain == 'yes') print ' selected="selected"';?>><?php _e('Yes', 'cawpsgoogleanalytics');?></option>
+						<option value="no"<?php if($sub_domain == 'no') print ' selected="selected"';?>><?php _e('No', 'cawpsgoogleanalytics');?></option> 
+					</select>
+				</td>
+			</tr>
+			<tr height="35">
+				<td>
+					<h3><?php _e('Sub domain URL?', 'cawpsgoogleanalytics');?>:</h3>
+				</td>
+				<td>
+					<input type="text" name="frm_sub_domain_url" size="50" value="<?php echo get_option('cawps_ga_sub_domain_url');?>"/>
+				</td>
+			</tr>
+			<tr height="35">
+				<td>
+					<h3><?php _e('Allow multiple top-level domains?', 'cawpsgoogleanalytics');?>:</h3>
+					<p><?php _e('Examples: www.yoursite.com -and- www.yoursite.co.uk', 'cawpsgoogleanalytics');?></p>
+				</td>
+				<td>
+					<select name="frm_multiple_top_level" style="width:75px;">
+						<option value="yes"<?php if($allow_multiple_domain == 'yes') print ' selected';?>><?php _e('Yes', 'cawpsgoogleanalytics');?></option>
+						<option value="no"<?php if($allow_multiple_domain == 'no') print ' selected';?>><?php _e('No', 'cawpsgoogleanalytics');?></option> 
+					</select>
+				</td>
+			</tr>
+		</table>
+		<input type="submit" name="frm_submit" class="button button-primary" value="<?php _e('Save', 'cawpsgoogleanalytics');?>"/>
+		</form>
+	</div>
+<?php
+}
+?>
